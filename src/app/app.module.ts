93c28519fce46b0c9eed6044a2f6f8ca1cd02ec5
Eugen <doarsajoc@gmail.com>
2020-12-20 01:08:45 +0200

    Ziua 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ComponentsModule} from './components/components.module';
+import {SimplePagesModule} from './simple-pages/simple-pages.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    SimplePagesModule,
+    ComponentsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
